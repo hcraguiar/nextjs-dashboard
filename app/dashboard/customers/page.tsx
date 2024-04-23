70d2e2c6ccd60ec2dd05ec1838fa6a4caef1e3b9
Helder Aguiar <hcraguiar@gmail.com>
2024-04-23 14:43:05 -0300

    FEAT: Customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,18 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Customers() {
-  return <p>Customers Page</p>
+export default async function Customers({ searchParams }: { searchParams?: {query?: string, page?: string}}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  )
 }
